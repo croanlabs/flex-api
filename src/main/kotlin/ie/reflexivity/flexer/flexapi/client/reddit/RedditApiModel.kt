ebb74408871adfe0e2630c4ef449bf128952ea41
JohnnyB <john.baxter@erstegroup.com>
2017-10-06 18:20:47 +0200

    AD-8 reddit client

--- src/main/kotlin/ie/reflexivity/flexer/flexapi/client/reddit/RedditApiModel.kt
+++ src/main/kotlin/ie/reflexivity/flexer/flexapi/client/reddit/RedditApiModel.kt
@@ -1,3 +1,41 @@
 package ie.reflexivity.flexer.flexapi.client.reddit
 
 
+data class SubRedditListing(
+        val kind: String,
+        val data: SubRedditData
+)
+
+data class SubRedditData(
+        val children: List<SubRedditChild>,
+        val after: String,
+        val before: String
+)
+
+data class SubRedditChild(
+        val kind: String,
+        val data: SubRedditChildData
+)
+
+data class SubRedditChildData(
+        val domain: String,
+        val id: String,
+        val title: String,
+        val subreddit_id: String,
+        val url: String,
+        val author: String,
+        val created: Long,
+        val name: String,
+        val view_count: Int,
+        val num_crossposts: Int,
+        val score: Int,
+        val ups: Int,
+        val num_comments: Int
+)
+
+data class RedditModerator(
+        val author_flair_text: String,
+        val name: String,
+        val date : Long,
+        val id : String
+)
--- src/main/kotlin/ie/reflexivity/flexer/flexapi/client/reddit/RedditClient.kt
+++ src/main/kotlin/ie/reflexivity/flexer/flexapi/client/reddit/RedditClient.kt
@@ -3,7 +3,16 @@ package ie.reflexivity.flexer.flexapi.client.reddit
 import com.google.gson.GsonBuilder
 import ie.reflexivity.flexer.flexapi.client.github.GsonDateTimeConverter
 import ie.reflexivity.flexer.flexapi.client.reddit.api.SubredditApiService
+import okhttp3.Credentials
+import okhttp3.Interceptor
+import okhttp3.Interceptor.Chain
 import okhttp3.OkHttpClient
+import okhttp3.Response
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod.POST
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.client.RestTemplate
 import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -15,8 +24,13 @@ fun main(args: Array<String>) {
     val gson = GsonBuilder()
             .registerTypeAdapter(LocalDateTime::class.java, GsonDateTimeConverter())
             .create()
-    val okHttpClient = OkHttpClient().newBuilder().build()
     val gsonFactory = GsonConverterFactory.create(gson)
+
+    val okHttpClient = OkHttpClient()
+            .newBuilder()
+            .addInterceptor(RedditOAuthInterceptor())
+            .build()
+
     val retroFit = Retrofit.Builder()
             .baseUrl("https://oauth.reddit.com/")
             .addConverterFactory(gsonFactory as Converter.Factory)
@@ -24,9 +38,63 @@ fun main(args: Array<String>) {
             .build()
     val subredditService = retroFit.create(SubredditApiService::class.java)
 
-    val call = subredditService.fetchSubreddit("melonproject")
-    val response = call.execute()
+    var hasPages = true
+    var nextPageTag: String? = null
 
-    println(response.body())
+    var count = 0
+    while (hasPages) {
+        val call = subredditService.fetchSubreddit(subReddit = "melonproject", after = nextPageTag)
+        val response = call.execute()
+        val subredditListing = response.body()
+        nextPageTag = subredditListing!!.data.after
+        subredditListing.data.children.forEach {
+            println(it)
+            count++
+        }
+        if (nextPageTag == null) {
+            hasPages = false
+        }
+    }
+    println("Reddit has these number of articles $count")
+}
 
+class RedditOAuthInterceptor() : Interceptor {
+
+    private var redditToken: RedditToken? = null
+
+    override fun intercept(chain: Chain): Response {
+        if (redditToken == null || redditToken!!.isTokenExpired()) {
+            redditToken = fetchRedditOauthToken()
+            //redditToken = RedditToken(access_token = "cLWZJZtvRyD5PNk3WBkRlV-pyFU", token_type = "bearer", expires_in = 3600, scope = "*")
+        }
+        val originalRequest = chain.request()
+        val builder = originalRequest.newBuilder().header("Authorization",
+                "Bearer ${redditToken!!.access_token}")
+        val newRequest = builder.build()
+        return chain.proceed(newRequest)
+    }
+
+    private fun fetchRedditOauthToken(): RedditToken {
+        val basicAuth = Credentials.basic("_3QgWk2uSnqcmw", "jrIEieleMKPsvSnGULQXsOuEVE4")
+        val fetchToken = RestTemplate()
+        val headers = HttpHeaders()
+        headers.add(HttpHeaders.AUTHORIZATION, "$basicAuth")
+        headers.add(HttpHeaders.USER_AGENT, "This is the flex bot")
+        headers.add(HttpHeaders.CONTENT_TYPE, "application/x-www-form-urlencoded")
+        val bodyMap = LinkedMultiValueMap<String, String>()
+        bodyMap.add("grant_type", "client_credentials")
+        val request = HttpEntity(bodyMap, headers)
+        val response = fetchToken.exchange("https://www.reddit.com/api/v1/access_token", POST, request, RedditToken::class.java)
+        return response.body
+    }
+}
+
+data class RedditToken(
+        val access_token: String,
+        val token_type: String,
+        val expires_in: Int,
+        val scope: String,
+        val created: LocalDateTime = LocalDateTime.now()
+) {
+    fun isTokenExpired() = LocalDateTime.now().isBefore(created.plusSeconds(expires_in.toLong()))
 }
