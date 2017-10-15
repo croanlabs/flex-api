e0871a81d2da026d8b7c6f9b46d0094c90decb54
lovelyliatroim <j_baxter99@hotmail.com>
2017-10-15 15:57:44 +0200

    AD-21 use maven spotify plugin to create images, fixed logging (#11)

--- bin/docker-publish-local.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#Script is for building a docker image to run in a local docker environment.
-#Local docker environment is set to the test build profile.
-
-export BUILD_PROFILE=local
-
-bash ./bin/docker-publish-cloud.sh ${1}
